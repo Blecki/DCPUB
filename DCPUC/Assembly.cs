3a55ffb5d03cc1b6bcdfa0879800b31ce33ba424
Blecki <jm@omnisu.com>
2012-04-07 08:18:12 -0400

    Better branching assembly generation

---
+++ DCPUC/Assembly.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace DCPUC
+{
+    public class Instruction
+    {
+        public string ins;
+        public string a;
+        public string b;
+        public string comment;
+
+        public override string ToString()
+        {
+            if (ins[0] == ':' || ins == "BRK") return ins + (String.IsNullOrEmpty(comment) ? "" : (" ;" + comment));
+            else if (ins == "JSR") return ins + " " + a + (String.IsNullOrEmpty(comment) ? "" : (" ;" + comment));
+            else return ins + " " + a + ", " + b + (String.IsNullOrEmpty(comment) ? "" : (" ;" + comment));
+        }
+    }
+
+    public class Assembly
+    {
+        public List<Instruction> instructions = new List<Instruction>();
+        public bool barrierFlag = false;
+
+        public void Add(string ins, string a, string b, string comment = null)
+        {
+            var instruction = new Instruction();
+            instruction.ins = ins;
+            instruction.a = a;
+            instruction.b = b;
+            instruction.comment = comment;
+
+            if (barrierFlag)
+            {
+                barrierFlag = false;
+                instructions.Add(instruction);
+            }
+            else
+            {
+                bool ignore = false;
+                if (instructions.Count > 0)
+                {
+                    var lastIns = instructions[instructions.Count - 1];
+                    if (lastIns.ins == "SET" && instruction.ins == "SET" && instruction.b == "POP")
+                    {
+                        if (lastIns.a == "PUSH")
+                        {
+                            lastIns.a = instruction.a;
+                            ignore = true;
+                        }
+                    }
+                    else if (lastIns.ins == "SET" && instruction.ins == "SET" 
+                        && instruction.b == lastIns.a && instruction.b != "PUSH")
+                    {
+                        lastIns.a = instruction.a;
+                        ignore = true;
+                    }
+                }
+                if (!ignore) instructions.Add(instruction);
+            }
+
+        }
+
+        public void Barrier() { barrierFlag = true; }
+
+        
+    }
+}
---
+++ DCPUC/ComparisonNode.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Irony.Interpreter.Ast;
+
+namespace DCPUC
+{
+    class ComparisonNode : CompilableNode
+    {
+        private static Dictionary<String, String> opcodes = null;
+
+        public override void Init(Irony.Parsing.ParsingContext context, Irony.Parsing.ParseTreeNode treeNode)
+        {
+            base.Init(context, treeNode);
+            AddChild("Parameter", treeNode.ChildNodes[0]);
+            AddChild("Parameter", treeNode.ChildNodes[2]);
+            this.AsString = treeNode.ChildNodes[1].FindTokenAndGetText();
+        }
+
+        public override void Compile(Assembly assembly, Scope scope, Register target)
+        {
+            //Evaluate in reverse in case both need to go on the stack
+            var secondTarget = scope.FindFreeRegister();
+            if (Scope.IsRegister((Register)secondTarget)) scope.UseRegister(secondTarget);
+            (ChildNodes[1] as CompilableNode).Compile(assembly, scope, (Register)secondTarget);
+                        
+            if (AsString == "==" || AsString == "!=")
+            {
+                (ChildNodes[0] as CompilableNode).Compile(assembly, scope, Register.STACK);
+                if (target == Register.STACK)
+                {
+                    assembly.Add("SET", "A", "0x0", "Equality onto stack");
+                    assembly.Add((AsString == "==" ? "IFE" : "IFN"), "POP", Scope.GetRegisterLabelSecond(secondTarget));
+                    assembly.Add("SET", "A", "0x1");
+                    assembly.Add("SET", "PUSH", "A");
+                }
+                else
+                {
+                    assembly.Add("SET", Scope.GetRegisterLabelFirst((int)target), "0x0",  "Equality into register");
+                    assembly.Add((AsString == "==" ? "IFE" : "IFN"), "POP", Scope.GetRegisterLabelSecond(secondTarget));
+                    assembly.Add("SET", Scope.GetRegisterLabelFirst((int)target), "0x1");
+                }
+            }
+            
+            if (secondTarget == (int)Register.STACK)
+                scope.stackDepth -= 1;
+            else
+                scope.FreeRegister(secondTarget);
+        }
+    }
+
+    
+}
