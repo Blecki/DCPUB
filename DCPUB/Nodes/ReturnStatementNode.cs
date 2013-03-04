c93a591f8123c8b939f99d3537998c13dc50b526
Blecki <jm@omnisu.com>
2013-03-03 20:29:08 -0500

    Emit short literals; optimization to member-access.

--- DCPUB/Nodes/ReturnStatementNode.cs
+++ DCPUB/Nodes/ReturnStatementNode.cs
@@ -11,24 +11,28 @@ namespace DCPUB
         public override void Init(Irony.Parsing.ParsingContext context, Irony.Parsing.ParseTreeNode treeNode)
         {
             base.Init(context, treeNode);
-            AddChild("value", treeNode.ChildNodes[1]);
+            if (treeNode.ChildNodes[1].FirstChild.ChildNodes.Count > 0)
+                AddChild("value", treeNode.ChildNodes[1].FirstChild.FirstChild);
             this.AsString = treeNode.FindTokenAndGetText();
         }
 
         public override void AssignRegisters(CompileContext context, RegisterBank parentState, Register target)
         {
-            if (parentState.RegisterInUse(Register.B)) throw new CompileError("Sanity check failed. Return not at top level.");
-            this.target = Register.B; //parentState.FindAndUseFreeRegister();
-            parentState.UseRegister(Register.B);
-            Child(0).AssignRegisters(context, parentState, this.target);
-            parentState.FreeRegister(Register.B);
+            if (ChildNodes.Count > 0)
+            {
+                if (parentState.RegisterInUse(Register.B)) throw new CompileError("Sanity check failed. Return not at top level.");
+                this.target = Register.B; //parentState.FindAndUseFreeRegister();
+                parentState.UseRegister(Register.B);
+                Child(0).AssignRegisters(context, parentState, this.target);
+                parentState.FreeRegister(Register.B);
+            }
             //parentState.FreeMaybeRegister(this.target);
         }
 
         public override void ResolveTypes(CompileContext context, Scope enclosingScope)
         {
             base.ResolveTypes(context, enclosingScope);
-            if (Child(0).ResultType != enclosingScope.activeFunction.ResultType)
+            if (ChildNodes.Count > 0 && Child(0).ResultType != enclosingScope.activeFunction.ResultType)
                 context.AddWarning(Span, CompileContext.TypeWarning(Child(0).ResultType, enclosingScope.activeFunction.ResultType));
         }
 
@@ -37,9 +41,12 @@ namespace DCPUB
             var r = new Assembly.StatementNode();
             r.AddChild(new Assembly.Annotation(context.GetSourceSpan(this.Span)));
 
-            r.AddChild(Child(0).Emit(context, scope));
-            if (target != Register.A) r.AddInstruction(Assembly.Instructions.SET, Operand("A"), 
-                Operand(Scope.GetRegisterLabelSecond((int)target)));
+            if (ChildNodes.Count > 0)
+            {
+                r.AddChild(Child(0).Emit(context, scope));
+                if (target != Register.A) r.AddInstruction(Assembly.Instructions.SET, Operand("A"),
+                    Operand(Scope.GetRegisterLabelSecond((int)target)));
+            }
             r.AddChild(scope.activeFunction.CompileReturn(context, scope));
 
             return r;
